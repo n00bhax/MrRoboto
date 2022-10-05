9d309e9fab174e6b5a94b57a85625df4c8ba1557
Willem <willem.vanderelst@icloud.com>
2022-10-05 16:25:43 +0200

    new plots

--- mr_roboto/scripts/plots.py
+++ mr_roboto/scripts/plots.py
@@ -51,5 +51,170 @@ def plot2():
     plt.savefig('acceleration')
 
 
+def find_nearest(value, array):
+    arg = (np.abs(array - value)).argmin()
+    return arg
+
+def export_shortened_adc():
+    t, x, y, z, adc, adc_t = [], [], [], [], [], []
+
+    with open('acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+            z.append(float(row[2]))
+            t.append(float(row[3]))
+
+    with open('adc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            adc.append(float(row[0]))
+            adc_t.append(float(row[1]))
+
+
+
+    shortened_adc = []
+
+    for timestamp in t:
+        arg = find_nearest(timestamp, np.array(adc_t))
+        shortened_adc.append(adc[arg])
+
+    np.savetxt('./shortened_acc.csv', shortened_adc, fmt='%d')
+def plot3():
+    t, x, y, z, adc, adc_t = [], [], [], [], [], []
+
+    with open('acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+            z.append(float(row[2]))
+            t.append(float(row[3]))
+
+    with open('shortened_acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            adc.append(float(row[0]))
+
+    plt.xlabel("x-Acceleration (m/s^2 ?)")
+    plt.ylabel("Tactile force")
+
+    plt.ylim(0, 1023)
+    plt.xlim(-20, 20)
+
+    #tijdelijk alleen met acc_x om te zien of dit iets opleverd
+    plt.scatter(x, adc, color="red", s=0.1, marker='.')
+
+
+    plt.savefig('all3')
+
+
+def plot4():
+    t, x, y, z, adc, adc_t = [], [], [], [], [], []
+
+    with open('acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+            z.append(float(row[2]))
+            t.append(float(row[3]))
+
+    with open('shortened_acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            adc.append(float(row[0]))
+
+    plt.xlabel("x-Acceleration (m/s^2 ?)")
+    plt.ylabel("Tactile force")
+
+    plt.ylim(0, 1023)
+    plt.xlim(-20, 20)
+
+    adc = (np.array(adc))[2666: 8300]
+    x = (np.array(x))[2666:8300]
+
+    plt.title("Cardboard")
+
+    #tijdelijk alleen met acc_x om te zien of dit iets opleverd
+    plt.scatter(x, adc, color="red", s=0.1, marker='.')
+
+    plt.savefig('cardboard')
+
+def plot5():
+    t, x, y, z, adc, adc_t = [], [], [], [], [], []
+
+    with open('acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+            z.append(float(row[2]))
+            t.append(float(row[3]))
+
+    with open('shortened_acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            adc.append(float(row[0]))
+
+    plt.xlabel("x-Acceleration (m/s^2 ?)")
+    plt.ylabel("Tactile force")
+
+    plt.ylim(0, 1023)
+    plt.xlim(-20, 20)
+
+    adc = (np.array(adc))[10900:14500]
+    x = (np.array(x))[10900:14500]
+
+    #tijdelijk alleen met acc_x om te zien of dit iets opleverd
+    plt.scatter(x, adc, color="red", s=0.1, marker='.')
+
+    plt.title("Plushie")
+
+    plt.savefig('plushie')
+
+def plot6():
+    t, x, y, z, adc, adc_t = [], [], [], [], [], []
+
+    with open('acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+            z.append(float(row[2]))
+            t.append(float(row[3]))
+
+    with open('shortened_acc.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=';')
+
+        for row in plots:
+            adc.append(float(row[0]))
+
+    plt.xlabel("x-Acceleration (m/s^2 ?)")
+    plt.ylabel("Tactile force")
+
+    plt.ylim(0, 1023)
+    plt.xlim(-20, 20)
+
+    adc = (np.array(adc))[17400:22000]
+    x = (np.array(x))[17400:22000]
+
+    #tijdelijk alleen met acc_x om te zien of dit iets opleverd
+    plt.scatter(x, adc, color="red", s=0.1, marker='.')
+
+    plt.title("Hard Metal Speaker")
+
+    plt.savefig('speaker')
+
 if __name__ == '__main__':
-    plot2()
+    plot6()
